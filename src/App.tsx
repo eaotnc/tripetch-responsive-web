4960240c42a4a03c26f7ec7abc17fe37cb6b9687
eaogithub <bonsai.eao@gmail.com>
2023-11-15 03:52:14 +0700

    feat: make mobile responsive and add react slick

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+
 import "./App.scss";
+import "./slick.scss";
 import rugby from "./assets/rugby-desktop.png";
 import rugbyTablet from "./assets/rugby-tablet.png";
 import rugbyMobile from "./assets/rugby-mobile.png";
@@ -11,13 +13,13 @@ import bassMobile from "./assets/bass-mobile.png";
 function App() {
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
-    height: window.innerHeight,
+    height: window.innerWidth,
   });
 
   const handleResize = () => {
     setWindowSize({
       width: window.innerWidth,
-      height: window.innerHeight,
+      height: window.innerWidth,
     });
   };
 
@@ -41,18 +43,12 @@ function App() {
       return <img className="image-right" src={bass} />;
     } else if (windowSize.width > 720 && windowSize.width <= 1920) {
       return <img className="image-right-tablet" src={bassTablet} />;
-    } else {
-      // return <img className="image-right-mobile" src={bassMobile} />;
     }
   };
-  return (
-    <div>
-      <div className="content">
-        {renderRugbyImageByScreenSize()}
-        <div className="main-header-right">ATHLETS</div>
-        {windowSize.width && (
-          <img className="image-left-mobile" src={rugbyMobile} />
-        )}
+
+  const renderPcAndTabletContentAthlete = () => {
+    return (
+      <>
         <div className="section-right">
           <div className="sub-header">
             <div>
@@ -92,12 +88,50 @@ function App() {
             Access to training classes, tutor sessions, etc
           </div>
         </div>
+      </>
+    );
+  };
+  const renderAtheleteMobileContent = () => {
+    return (
+      <>
+        <div className="section-right second-color">
+          <div className="sub-header">
+            <div>
+              <div className="number-text">02</div>
+              <div className="number-underline" />
+            </div>
+            COLLABORATION
+          </div>
+          <div className="desc">
+            Work with other student athletes to increase visability. When you
+            share and like other players videos it will increase your visability
+            as a player. This is the team work aspect to Surface 1.
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="text-black">
+      {windowSize.width}
+      <div className="content">
+        {renderRugbyImageByScreenSize()}
+        <div className="main-header-right">ATHLETS</div>
+        {windowSize.width < 720 && (
+          <img className="image-left-mobile" src={rugbyMobile} />
+        )}
+        {windowSize.width > 720
+          ? renderPcAndTabletContentAthlete()
+          : renderAtheleteMobileContent()}
+
+        <div style={{ width: "500px" }}></div>
       </div>
 
       <div className="content">
         {renderBassImageByScreenSize()}
         <div className="main-header-left">PLAYERS</div>
-        {windowSize.width && (
+        {windowSize.width < 720 && (
           <img className="image-right-mobile" src={bassMobile} />
         )}
         <div className="section-left">
